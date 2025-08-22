2f4bdcbeae4aa177617bff2056d4613a32ba1124
ISAAC KWAME DOE <Kwamedoe1817@gmail.com>
2025-08-22 04:46:19 -0700

    Removal and update of files

--- Backend/applications/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('apprenticeship/', views.apprenticeship_view, name='apprenticeship'),
-    path('appointment/', views.appointment_view, name='appointment'),
-]
