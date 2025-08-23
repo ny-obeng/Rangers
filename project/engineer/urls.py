cd4d098b277d91a892985b0f9a64e6177b3b1def
Elton Koomson <eltonkoomson1400@gmail.com>
2025-08-23 22:28:33 +0000

    changes

--- project/engineer/urls.py
+++ project/engineer/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),  # homepage
+    path("services/", views.book_service, name="services"),  # booking form
     path("about/", views.about, name="about"),  # about us
     path("career/", views.apply_training, name="career"),  # training form
     path('booking/', views.booking_view, name = 'booking'),
-    path('booking/success/', views.booking_success, name='booking_success'),
-    path('application/success/', views.application_success, name = 'application_success'),
+    path('booking/success', views.booking_success, name='booking_success'),
+    path('application/success', views.application_success, name = 'application_success'),
 ]
