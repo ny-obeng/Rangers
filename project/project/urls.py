8e3f4fd5992722f18daada7d31201d7c44419462
Elton Koomson <eltonkoomson1400@gmail.com>
2025-08-23 20:30:37 +0000

    debug 2

--- project/project/urls.py
+++ project/project/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('engineer.urls')),
+    path('', include('project.engineer.urls')),
 ]
